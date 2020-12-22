b4cbc50a6d37bdba0f4f535455b793142f2e89ae
Tristan Toupin <tristantoupin@gmail.com>
2020-12-22 09:55:13 -0500

    fix(app): added forgoten files

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/get_cost')
+def get_cost():
+    return 'This piping will be very expensive my guy!'
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="8080")
