14cf83c7769910247f1fdfcd6bbf9e3b1d55edd8
Gabriel Charland <g.charland1@protonmail.com>
2021-01-12 11:00:09 -0500

    detect bounding boxes coortinates. Need to filter the ones that are not in line with the table

--- image_reader.py
+++ image_reader.py
@@ -1,6 +1,7 @@
 import pytesseract
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
 # if os == Windows:
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -23,8 +24,15 @@ class ImageReader:
         gray_img =  cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         return gray_img
 
-    def blur(self, img):
-        blur = cv2.GaussianBlur(img, (5, 5), 0)
+    def blur(self, img, area=(5, 5)):
+        w = area[0]
+        h = area[1]
+        if w % 2 == 0:
+            w = w + 1
+        if h % 2 == 0:
+            h = h + 1
+
+        blur = cv2.GaussianBlur(img, (w, h), 0)
         return blur
 
     def threshold(self, img, adaptive=True):
@@ -55,54 +63,42 @@ class ImageReader:
 
         return level_contours
 
-    def mask_outer_perimeter(self, shape, contours, min_area):
-        best_contour = None
+    def find_biggest_contour(self, img):
+        contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         max_area = 0
+        biggest_blob = None
         for c in contours:
             area = cv2.contourArea(c)
-            if area > min_area:
-                if area > max_area:
-                    max_area = area
-                    best_contour = c
+            if  area > max_area:
+                biggest_blob = c
+                max_area = area
 
+        return biggest_blob
 
-        mask = np.zeros(shape, np.uint8)
-        cv2.drawContours(mask, [best_contour], 0, 255, -1)
-        cv2.drawContours(mask, [best_contour], 0, 0, 2)
-        return mask
+    def filter_table(self, box_coordinates):
+        return box_coordinates
 
-    def draw_coutours(self, img, contours, min_area = 500):
-        c = 0
-        for cnt in contours:
-            if cv2.contourArea(cnt) > min_area:
-                cv2.drawContours(img, contours, c, (0,255,0), 3)
-            c += 1
-        return img
-
-    def isolate_outer_box_content(self, img, min_area = 500):
-        gray = self.grayscale(img)
-        blur = self.blur(gray)
-        thresh = self.threshold(blur, True)
-        contours = self.find_contours(thresh)
-        mask = self.mask_outer_perimeter(gray.shape, contours, min_area)
-
-        out = np.zeros_like(gray)
-        out[mask == 255] = gray[mask == 255]
-        return out
-
-SAVE_INTERMEDIATE = True
-
-if __name__ == '__main__':
-    image_reader = ImageReader(SAVE_INTERMEDIATE)
-    for n in [0, 1, 2]:
-        img = image_reader.load_image(f'./input/iso{n}.png', False)
-        print(f'./input/iso{n}.png')
+    def image_to_table(self, image_file):
+        img = image_reader.load_image(image_file, False)
         gray = image_reader.grayscale(img)
         blur = image_reader.blur(gray)
         thresh = image_reader.threshold(blur, True)
 
-        for level in [0, 1]:
-            contours = image_reader.find_contours_by_hierarchy(thresh, level)
+        hierarchy_level = 1
+        contours = image_reader.find_contours_by_hierarchy(thresh, hierarchy_level)
+        box_coordinates = []
+        for c in contours:
+            box_coordinates.append(cv2.boundingRect(c))
 
-            cv2.drawContours(img, contours, -1, (0,255,0), 2)
-            cv2.imwrite(f'./out/iso{n}_level{level}.png', img)
+        box_coordinates = self.filter_table(np.array(box_coordinates, dtype='uint8'))
+
+        return box_coordinates
+
+
+SAVE_INTERMEDIATE = False
+
+if __name__ == '__main__':
+    image_reader = ImageReader(SAVE_INTERMEDIATE)
+    for n in [0, 1, 2]:
+        print(image_reader.image_to_table(f'./input/iso{n}.png'))
+
