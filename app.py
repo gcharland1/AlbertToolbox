c7b80b5e4446c4bc3ab93f03631af46f703981ba
Gabriel Charland <g.charland1@protonmail.com>
2021-01-19 12:20:59 -0500

    Setupping html templates and forms

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+app = flask.Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return flask.render_template("index.html")
 
-@app.route('/<name>')
-def user(name):
-    return f'Hello {name}'
-
-@app.route('/get_cost')
+@app.route('/piping_estimator')
 def get_cost():
-    return 'This piping will be very expensive my guy!'
+    return flask.render_template("piping_estimator.html")
+
+@app.route('/tools')
+def show_tools():
+    return flask.render_template("our_tools.html")
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="8080")
+    app.run(host="0.0.0.0", port="8080", debug=True)
---
+++ forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PassrowdField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)]]
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_passrowd = PassrowdField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)]]
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_passrowd = PassrowdField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
