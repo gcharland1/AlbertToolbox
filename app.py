da582e0d87bb719c055e003c47b74618ac896bec
Gabriel Charland <g.charland1@protonmail.com>
2021-01-19 13:53:22 -0500

    Fixed minor bugs, changed basic visual for form.

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 import flask
+from forms import RegistrationForm, LoginForm
+
 app = flask.Flask(__name__)
+app.config['SECRET_KEY'] = '4b40c79de13feb562a644158dff8035c'
 
 
 @app.route('/')
 def home():
     return flask.render_template("index.html")
 
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    return flask.render_template("register.html", title="Register", form=form)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    return flask.render_template("login.html", title="Login", form=form)
+
 @app.route('/piping_estimator')
 def get_cost():
-    return flask.render_template("piping_estimator.html")
+    return flask.render_template("piping_estimator.html", title="Estimateur de coûts")
 
 @app.route('/tools')
 def show_tools():
-    return flask.render_template("our_tools.html")
+    return flask.render_template("our_tools.html", title="Nos outils")
 
 
 if __name__ == '__main__':
