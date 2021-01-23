e680a33c4190c05a5817e75b7514ba9cbd85d22a
Gabriel Charland <g.charland1@protonmail.com>
2021-01-23 09:02:15 -0500

    Failled attempts to improve Piping Estimator Beta. Will move to seperate branch to improve feature

--- app/tools/controllers.py
+++ app/tools/controllers.py
@@ -1,33 +1,44 @@
 import flask
 
+from app import db
 from app.tools import forms
+from bin import estimator
 
 tools = flask.Blueprint('tools', __name__, )
 
+@tools.route('/piping_beta', methods=["GET", "POST"])
+def piping_beta(n_rows=10):
+    form = forms.BetaPipingForm()
+    if flask.request.method == "POST":
+        form_rows = form.rows
+        bom_rows = []
+        for r in form_rows:
+            bom_rows.append([r.item_field.data,
+                             r.diameter_field.data,
+                             r.schedule_field.data,
+                             r.material_field.data,
+                             r.quantity_field.data])
+        return str(bom_rows)
+    else:
+        for i in range(n_rows):
+            form.rows.append_entry()
+            form.n_rows += 1
+
+    return flask.render_template("tools/piping_beta.html", title="Estimateur Beta", form=form, n_rows=n_rows)
+
 @tools.route('/piping_estimator', methods=["GET", "POST"])
 def piping_estimator():
-    if "premium" in flask.session:
-        form = forms.PipingForm()
-        if flask.request.method == 'POST':
-            filename = form.file.data.filename
-            print(filename)
-            file_data = flask.request.files[form.file.name].read()
-            image_path = os.path.join(app.config["UPLOAD_PATH"], filename)
-            open(image_path, 'wb').write(file_data)
-            reader = image_reader.ImageReader()
-            bom_content = reader.image_to_text_table(image_path)
-            os.remove(image_path)
-            return flask.url_for("name", name=bom_content)
+    form = forms.PipingForm()
+    if flask.request.method == 'POST':
+        filename = form.file.data.filename
+        print(filename)
+        file_data = flask.request.files[form.file.name].read()
+        image_path = os.path.join(app.config["UPLOAD_PATH"], filename)
+        open(image_path, 'wb').write(file_data)
+        reader = image_reader.ImageReader()
+        bom_content = reader.image_to_text_table(image_path)
+        os.remove(image_path)
+        return flask.url_for("name", name=bom_content)
 
-        else:
-            return flask.render_template("tools/piping_estimator.html", title="Estimateur de coûts", form=form)
     else:
-        form = forms.BetaPipingForm()
-        if flask.request.method == "POST":
-            return "Success"
-        else:
-            for i in range(5):
-                form.rows.append_entry()
-                form.n_rows += 1
-
-        return flask.render_template("tools/piping_beta.html", title="Estimateur Beta", form=form)
+        return flask.render_template("tools/piping_estimator.html", title="Estimateur de coûts", form=form)
--- app/tools/forms.py
+++ app/tools/forms.py
@@ -1,19 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, FileField, SelectField, FieldList, FormField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField, SubmitField, FileField, SelectField, FieldList, FormField, BooleanField
+from wtforms.validators import DataRequired
 
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Nom d''utilisateur', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Mot de passe', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirmer mot de passe', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Créer mon compte')
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Mot de passe', validators=[DataRequired()])
-    submit = SubmitField('Se connecter')
 
 class PipingForm(FlaskForm):
     file = FileField('Fichier image', validators=[DataRequired()])
@@ -25,6 +13,7 @@ class BetaPipingItemForm(FlaskForm):
     schedule_list = ["10", "20", "40", "80"]
     material_list = ["Acier Carbone", "Innox"]
 
+    select_field = BooleanField("Sélectionner")
     item_field = SelectField("Type d'élément", choices=item_list, validators=[DataRequired()])
     diameter_field = SelectField("Diamètre (po)", choices=diameter_list, validators=[DataRequired()])
     schedule_field = SelectField("Schedule", choices=schedule_list, validators=[DataRequired()])
