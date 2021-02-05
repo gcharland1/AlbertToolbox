e7100e161f2ecbed6a00004a65d99ba93b2c14de
Gabriel Charland <g.charland1@protonmail.com>
2021-02-04 20:42:08 -0500

    Added basic profile page with dummy buttons

--- app/user_profile/controllers.py
+++ app/user_profile/controllers.py
@@ -1,5 +1,6 @@
 import flask
-
+from app import db
+from app.authentification import User
 from app.user_profile import forms
 
 user_profile = flask.Blueprint('user_profile', __name__)
@@ -8,9 +9,11 @@ user_profile = flask.Blueprint('user_profile', __name__)
 def profile_page():
     if 'user' in flask.session:
         username = flask.session['user']
-        return f"Success! {username}"
+        user = User.query.filter_by(name=username).first()
+
+        return flask.render_template("user_profile/profile_page.html", user=user)
     else:
-        msg = f"Vous n'êtes pas connecter. <a href={flask.url_for('auth.login')}>Se connecter?</a>"
+        msg = f"Vous n'êtes pas connecté. <a href={flask.url_for('auth.login')}>Se connecter?</a>"
         url = 'main.home'
 
         flask.flash(flask.Markup(msg), 'info')
