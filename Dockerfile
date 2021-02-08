d0037d364fd8b20b101a359748ddf28563746f0b
Gabriel Charland <g.charland1@protonmail.com>
2021-02-08 15:20:51 -0500

    Docker config

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM python:3.9-buster
 WORKDIR .
 COPY . .
 RUN pip install -r requirements.txt
+EXPOSE 8080
 ENTRYPOINT ["python"]
 CMD ["run.py" ]
--- config.py
+++ config.py
@@ -6,7 +6,7 @@ DEBUG = False
 # Define the application directory
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-# SERVER_NAME = "localhost"
+SERVER_NAME = "albert-toolbox.com"
 # Define the database - we are working with
 # SQLite for this example
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
