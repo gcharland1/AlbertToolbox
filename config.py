41274cf545c2fadfbdc000a0c1e55b8b83ff9ecc
Gabriel Charland <g.charland1@protonmail.com>
2021-03-02 15:47:16 -0500

    Fixed db reference issue

--- config.py
+++ config.py
@@ -5,17 +5,16 @@ DEBUG = False
 
 # Define the application directory
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
+print(BASE_DIR)
 # SERVER_NAME = "albert-toolbox.com"
 # Define the database - we are working with
 # SQLite for this example
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR + 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
 
 SQLALCHEMY_BINDS = {
-    'users':    'sqlite:///' + os.path.join(BASE_DIR + 'users.db'),
-    'clients':  'sqlite:///' + os.path.join(BASE_DIR + 'clients.db')
+    'users':    'sqlite:///' + os.path.join(BASE_DIR, 'users.db'),
+    'clients':  'sqlite:///' + os.path.join(BASE_DIR, 'clients.db')
 }
-
 # Application threads. A common general assumption is
 # using 2 per available processor cores - to handle
 # incoming requests using one and performing background
